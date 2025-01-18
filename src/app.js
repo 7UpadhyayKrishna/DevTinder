fdbaec93470becb67d08fd08ee3c0d265fe9de7c
Krishna upadhyay <krishnaupadhyay207@gmail.com>
2025-01-19 00:15:56 +0530

    exploring routing

--- src/app.js
+++ src/app.js
@@ -2,10 +2,23 @@ const express = require("express");
 
 const app = express();
 
-app.use("/test", (req, res) => {
-  res.send("hello f   ro   m the ser   ver");
+app.post("/test", (req, res) => {
+  res.send({ firstname: "krishna", lastname: "upadhyay" });
 });
 
+app.delete("/test", (req, res) => {
+  res.send("deleted tulshi");
+});
+
+app.use("/test", (req, res) => {
+  res.send("hello from the server test");
+});
+app.use("/gf", (req, res) => {
+  res.send("hello from the server");
+});
+// app.get("/test", (req, res) => {
+//   res.send({ firstname: "krishna", lastname: "upadhyay" });
+// });
 app.listen(3000, () => {
   console.log("serv     er is   listen");
 });
