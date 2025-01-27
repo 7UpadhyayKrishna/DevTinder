2966ec33fd25261d9b8557ec793a84a10db6c117
Krishna upadhyay <krishnaupadhyay207@gmail.com>
2025-01-27 21:12:40 +0530

    day4

--- src/app.js
+++ src/app.js
@@ -2,23 +2,8 @@ const express = require("express");
 
 const app = express();
 
-// app.post("/test", (req, res) => {
-//   res.send({ firstname: "krishna", lastname: "upadhyay" });
-// });
+// app.get("/");
 
-// app.delete("/test", (req, res) => {
-//   res.send("deleted tulshi");
-// });
-
-// app.use("/test", (req, res) => {
-//   res.send("hello from the server test");
-// });
-// app.use("/gf", (req, res) => {
-//   res.send("hello from the server");
-// });
-app.get("/test", (req, res) => {
-  res.send({ firstname: "krishna", lastname: "upadhyay" });
-});
 app.listen(3000, () => {
   console.log("serv     er is   listen");
 });
