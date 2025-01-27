7d4fc33434545dbc635b46fb516756dfe067a2ae
Krishna upadhyay <krishnaupadhyay207@gmail.com>
2025-01-28 01:27:23 +0530

    day7

--- src/app.js
+++ src/app.js
@@ -3,23 +3,74 @@ const connectdb = require("./config/database");
 const User = require("./models/user");
 const app = express();
 
-app.use(express.json()); //midleware for reading jason file
+app.use(express.json()); //middleware for reading jason file
 
+//API for updating user
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  try {
+    const user = await User.findByIdAndUpdate({ _id: userId }, data);
+    res.send("updated");
+  } catch (err) {
+    res.status(400).send("somthing went wrong");
+  }
+});
+
+//API for deleting user
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    const user = await User.findByIdAndDelete(userId);
+    if (user) {
+      res.send("deleted");
+    } else {
+      res.send("user not found");
+    }
+  } catch (err) {
+    res.status(400).send("somthing went wrong");
+  }
+});
+
+//API for geting all user on the database
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    console.log(users);
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+//API for geting user on the database by emailId
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+
+  try {
+    const user = await User.find({ emailId: userEmail });
+    if (user.length === 3) {
+      res.send(user);
+      // console.log(user);
+    } else {
+      res.status(400).send("user not found");
+    }
+  } catch (err) {
+    res.status(400).send("somthing went wrong");
+  }
+});
+
+//API for insert user on the database
 app.post("/signup", async (req, res) => {
-  console.log(req.body);
+  // console.log(req.body);
 
-  // const user = new User({
-  //   firstName: "krishna2",
-  //   lastName: "upadhyay2",
-  //   emailId: "krishnaupadhyay207@gmail.com",
-  //   password: "Ram@161003",
-  // });
-  // try {
-  //   await user.save();
-  //   res.send("data saved");
-  // } catch (err) {
-  //   res.status(400).send("data not saved");
-  // }
+  const user = new User(req.body);
+  try {
+    await user.save();
+    res.send("data saved");
+  } catch (err) {
+    res.status(400).send("data not saved");
+  }
 });
 
 connectdb()
