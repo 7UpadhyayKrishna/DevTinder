b98fe14886898e6cc27f1fb07a99ff7bbe0e4c56
Krishna upadhyay <krishnaupadhyay207@gmail.com>
2025-01-28 21:30:33 +0530

    day9

--- src/models/user.js
+++ src/models/user.js
@@ -1,25 +1,43 @@
+const { Timestamp } = require("mongodb");
 const mongoose = require("mongoose");
+var validator = require("validator");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    emailId: {
+      type: String,
+      required: true,
+      unique: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Invalid email id");
+        }
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      default: "18",
+    },
+    gender: {
+      type: String,
+    },
   },
-  lastName: {
-    type: String,
-  },
-  emailId: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const User = mongoose.model("User", userSchema);
 
